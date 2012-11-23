c252ebedb00c100dc224a644aa506ccde55f3ef1
Riley Eynon-Lynch <riley@activegrade.com>
2012-11-23 15:48:08 -0600

    Simplify: let teachers get right to creating a new prompt instead of making them read a splash screen

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Activeprompt::Application.routes.draw do
 
   match ":url", :controller => 'prompts', :action => 'show_teacher_view', :url => /[tT][a-zA-Z]+/
   match ":url", :controller => 'prompts', :action => 'show_student_view', :url => /[sS][a-zA-Z]+/
-  
-  get "home/index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -58,7 +56,7 @@ Activeprompt::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
 
-root :to => 'home#index'
+root :to => 'prompts#new'
 
   # See how all your routes lay out with "rake routes"
 
